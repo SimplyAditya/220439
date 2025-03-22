29fe2a7bcd21067458217e2919d03513a9658bab
SimplyAditya <adityabansal200311@gmail.com>
2025-03-22 11:28:25 +0530

    Completed first question

--- averageCalculator/controllers/index.js
+++ averageCalculator/controllers/index.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const WINDOW_SIZE = 5;
 const API_URL = process.env.API_URL || "http://20.244.56.144";
 
-let numbers_window = [];
+let numbersWindow = [];
 let token = null;
 let expiryTime = 0;
 
@@ -34,16 +34,24 @@ const fetchNumbers = async (numberid) => {
   try {
     const temp_token = await fetchToken();
     let url = null;
-    numberid === 'p' ? url = `${API_URL}/test/primes` : numberid === 'f' ? url = `${API_URL}/test/fibo` : numberid === 'e' ?url = `${API_URL}/test/even` : numberid === 'r' ? url = `${API_URL}/test/rand` : null;
-    if(url){
-        const response = await axios.get(url , {
-          headers: {
-            Authorization: `Bearer ${temp_token}`,
-          },
-        });
-        return response.data.numbers;
+    numberid === "p"
+      ? (url = `${API_URL}/test/primes`)
+      : numberid === "f"
+      ? (url = `${API_URL}/test/fibo`)
+      : numberid === "e"
+      ? (url = `${API_URL}/test/even`)
+      : numberid === "r"
+      ? (url = `${API_URL}/test/rand`)
+      : null;
+    if (url) {
+      const response = await axios.get(url, {
+        headers: {
+          Authorization: `Bearer ${temp_token}`,
+          timeout: 500,
+        },
+      });
+      return response.data.numbers;
     }
-    
   } catch (error) {
     console.error("Error fetching numbers:", error);
     throw error;
@@ -51,13 +59,34 @@ const fetchNumbers = async (numberid) => {
 };
 
 const calculateAverage = async (req, res) => {
-  const { numberid } = req.params;
-  const numbers = await fetchNumbers(numberid);
-  console.log("Number ID:", numbers);
-  res.status(200).json({
-    status: "success",
-    message: "Average calculated successfully",
-  });
+  try {
+    const { numberid } = req.params;
+    console.log("Number ID:", numberid);
+    const numbers = await fetchNumbers(numberid);
+    const windowPrevState = numbersWindow;
+    numbersWindow = [...numbersWindow, ...numbers].slice(-WINDOW_SIZE);
+    const windowCurrState = numbersWindow;
+    const avg =
+      numbersWindow.length > 0
+        ? (
+            numbersWindow.reduce((sum, num) => sum + num, 0) /
+            numbersWindow.length
+          ).toFixed(2)
+        : 0.0;
+
+    res.status(200).json({
+      windowPrevState,
+      windowCurrState,
+      numbers,
+      avg,
+    });
+  } catch (error) {
+    console.error("Error in calculateAverage:", error);
+    return res.status(500).json({
+      status: "fail",
+      message: "Internal server error",
+    });
+  }
 };
 
 export default calculateAverage;
